e6809170a2e560f72cb75549ba243b84f542d3a3
Rey Diaz <reybdiazjr@gmail.com>
2019-09-24 20:35:31 -0400

    wiring up requests in the liri.js file

--- liri.js
+++ liri.js
@@ -1,6 +1,22 @@
-// 
+// REQUIRE .env File to link to liri.js
 require("dotenv").config();
-var keys = require("./keys");
+
+// REQUIRE request to activate api keys in liri.js utilizing const because it wont change
+const keys = require("./keys");
+
+// REQUIRE fs File System utilizing const because it will never change
+const fs = require("fs");
+
+// REQUIRE request
+let request = require("request");
+
+// REQUIRE moment
+const moment = require("moment");
+
+
+
+
+
 // var spotify = new Spotify(keys.spotify);
 
 // Make it so liri.js can take in one of the following commands:
